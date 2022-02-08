40b6c4c11124725af38f3278f208cfd3e1fbb88f
Mustafa <must99@inbox.ru>
2022-02-08 14:19:05 +0300

    Commit

---
+++ Map/Map/DescriptionViewController.swift
@@ -0,0 +1,68 @@
+//
+//  DescriptionViewController.swift
+//  Map
+//
+//  Created by Nikita on 04.02.2022.
+//
+
+import UIKit
+import MapKit
+
+class DescriptionViewController: UIViewController {
+
+    var tableView2 = UITableView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: nil)
+        //self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: self, action: #selector(funct))
+        //self.navigationItem.leftBarButtonItem = UIBarButtonItem
+        self.navigationItem.leftBarButtonItem
+        setupTableView()
+    }
+                                                                 
+    @objc func funct() {
+        print("qwe")
+    }
+                                                                 
+                                                                 
+    
+    func setupTableView() {
+        //tableView2 = UITableView(frame: view.bounds, style: .plain)
+        tableView2 = UITableView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        tableView2.register(TableViewCellOne.self, forCellReuseIdentifier: "TableViewCellOne")
+        tableView2.separatorStyle = .none
+        tableView2.clipsToBounds = false
+        tableView2.dataSource = self
+        tableView2.delegate = self
+        tableView2.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(self.tableView2)
+    }
+
+}
+
+extension DescriptionViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell: TableViewCellOne = tableView2.dequeueReusableCell(withIdentifier: "TableViewCellOne") as! TableViewCellOne
+        cell.configure(with: "")
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    
+}
+
+extension DescriptionViewController: UITableViewDelegate {}
---
+++ Map/Map/MapViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MapViewController.swift
+//  Map
+//
+//  Created by Nikita on 03.02.2022.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    var map = MKMapView()
+    var selectedAnnotation: MKPointAnnotation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupMap()
+    }
+    
+    
+    func setupMap() {
+        map = MKMapView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        let location = CLLocationCoordinate2DMake(55.757962, 37.611006)
+        let span = MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
+        let region = MKCoordinateRegion(center: location, span: span)
+        map.setRegion(region, animated: true)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "School"
+        annotation.subtitle = "Ooh"
+        map.addAnnotation(annotation)
+        
+        map.delegate = self
+        //map.mapClick = { coordinates in
+        //    let latitude = coordinates.latitude
+        //}
+        
+        //map.target(forAction: #selector(handleClick), withSender: .)
+        
+        view.addSubview(map)
+    }
+
+    @objc func handleClick() {
+        Navigation.nav.viewControllers = [DescriptionViewController()]
+    }
+}
+
+
+extension MapViewController: MKMapViewDelegate {
+    
+    
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        //self.selectedAnnotation = view.annotation as? MKPointAnnotation
+        //Navigation.nav.viewControllers = [DescriptionViewController()]
+        Navigation.nav.pushViewController(DescriptionViewController(), animated: false)
+    }
+    
+}
